fad9c60f09c89e46f04689e40d2f015d667be4a7
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-09-02 19:06:05 -0300

    Upando arquivos

---
+++ cursojs/aula4/comentarios.js
@@ -0,0 +1,10 @@
+//Escrever um comentário de linha
+/* Esse é um comentário de bloco
+Onde eu consigo escrever em mais de uma liha */
+console.log('Olá, mundo!') //Aqui tem um comentário
+console.log('Linha 1')
+//Comentário
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+//Vai ser ignorado pelo navegador
