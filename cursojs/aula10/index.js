c22306a6f33a81b1833ec138eebce7d22db06228
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-09-03 22:47:22 -0300

    Create index.js

---
+++ cursojs/aula10/index.js
@@ -0,0 +1,3 @@
+//Tipos de dados primitivos 
+//null é quando eu quero que essa variável seja nula e não aponte para local nenhum da memória
+//undefined é um valor automatico quando também não aponta para local nenhum da memória
